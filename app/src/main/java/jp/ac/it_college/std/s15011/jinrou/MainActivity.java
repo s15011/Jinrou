92ba8298ec75c45c331d5d8eca3ae4eebd6ef666
nsk0308 <nsk19970308@gmail.com>
2017-03-19 18:43:46 +0900

    タイトルバー削除

--- app/src/main/java/jp/ac/it_college/std/s15011/jinrou/MainActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s15011/jinrou/MainActivity.java
@@ -1,11 +1,11 @@
 package jp.ac.it_college.std.s15011.jinrou;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.run_game_activity).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TitleActivity.class);
+                Intent intent = new Intent(getApplicationContext(), GameActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/jp/ac/it_college/std/s15011/jinrou/sam.java
+++
@@ -1,8 +0,0 @@
-package jp.ac.it_college.std.s15011.jinrou;
-
-/**
- * Created by samuel on 17/03/17.
- */
-
-public class sam {
-}
