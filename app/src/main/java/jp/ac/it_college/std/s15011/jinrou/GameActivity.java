dc89230c361e172d014c4161dc7d519a73e2debf
s15007 <s15007@std.it-college.ac.jp>
2017-03-21 17:08:09 +0900

    カウントダウンタイマー実装

--- app/src/main/java/jp/ac/it_college/std/s15011/jinrou/GameActivity.java
+++ app/src/main/java/jp/ac/it_college/std/s15011/jinrou/GameActivity.java
@@ -2,6 +2,7 @@ package jp.ac.it_college.std.s15011.jinrou;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -15,6 +16,8 @@ import android.widget.ToggleButton;
 public class GameActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     TabLayout tabLayout;
+    private String GameFont =  "JKG-L_3.ttf";
+    private String GameStatus = "night";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +34,9 @@ public class GameActivity extends AppCompatActivity implements ViewPager.OnPageC
         ToggleButton tb = (ToggleButton) findViewById(R.id.toggleButton);
 
         player_name.setText("なまえ");
-        day_or_night.setText("ひる");
+        day_or_night.setText("よる");
 
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "JKG-L_3.ttf");
-//        Typeface typeface = Typeface.createFromAsset(getAssets(), "03SmartFontUI.otf");
+        Typeface typeface = Typeface.createFromAsset(getAssets(), GameFont);
         player_name.setTypeface(typeface);
         player_job.setTypeface(typeface);
         day_or_night.setTypeface(typeface);
@@ -72,24 +74,9 @@ public class GameActivity extends AppCompatActivity implements ViewPager.OnPageC
         tabLayout.getTabAt(0).setIcon(R.drawable.tab_icon_selector1).setText("メンバー");
         tabLayout.getTabAt(1).setIcon(R.drawable.tab_icon_selector2).setText("チャット");
         changeTabsFont();
+        CountDown countDown = new CountDown(182000, 1000);
+        countDown.start();
     }
-
-    private void changeTabsFont() {
-        Typeface typeface = Typeface.createFromAsset(getAssets(), "JKG-L_3.ttf");
-        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
-        int tabsCount = vg.getChildCount();
-        for (int j = 0; j < tabsCount; j++) {
-            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
-            int tabChildsCount = vgTab.getChildCount();
-            for (int i = 0; i < tabChildsCount; i++) {
-                View tabViewChild = vgTab.getChildAt(i);
-                if (tabViewChild instanceof TextView) {
-                    ((TextView) tabViewChild).setTypeface(typeface);
-                }
-            }
-        }
-    }
-
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -106,34 +93,61 @@ public class GameActivity extends AppCompatActivity implements ViewPager.OnPageC
 
     }
 
-//    public static class TestFragment extends Fragment {
-//        public TestFragment() {
-//        }
-//
-//        public static TestFragment newInstance(int page) {
-//            Bundle args = new Bundle();
-//            args.putInt("page", page);
-//            TestFragment fragment = new TestFragment();
-//            fragment.setArguments(args);
-//            return fragment;
-//        }
-//
-//        @Nullable
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
-//            int page = getArguments().getInt("page", 1);
-//            View view;
-//            switch (page) {
-//                case 2:
-//                    view = inflater.inflate(R.layout.chat_tab, group, false);
-//                    break;
-//                default:
-//                    view = inflater.inflate(R.layout.member_tab, group, false);
-//                    break;
-//            }
-//            return view;
-//        }
-//    }
+    // タブのフォント変更
+    private void changeTabsFont() {
+        Typeface typeface = Typeface.createFromAsset(getAssets(), GameFont);
+        ViewGroup vg = (ViewGroup) tabLayout.getChildAt(0);
+        int tabsCount = vg.getChildCount();
+        for (int j = 0; j < tabsCount; j++) {
+            ViewGroup vgTab = (ViewGroup) vg.getChildAt(j);
+            int tabChildsCount = vgTab.getChildCount();
+            for (int i = 0; i < tabChildsCount; i++) {
+                View tabViewChild = vgTab.getChildAt(i);
+                if (tabViewChild instanceof TextView) {
+                    ((TextView) tabViewChild).setTypeface(typeface);
+                }
+            }
+        }
+    }
 
+    // 残り秒数のカウントダウン
+    public class CountDown extends CountDownTimer {
 
+        TextView count_txt = (TextView) findViewById(R.id.sec);
+        TextView d_or_n = (TextView) findViewById(R.id.day_or_night);
+        private long GameTime = 0;
+
+        public CountDown(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+            count_txt.setText(String.valueOf((millisUntilFinished-1000)/1000));
+        }
+
+        @Override
+        public void onFinish() {
+            switch (GameStatus) {
+                case "night":
+                    d_or_n.setText("ひる");
+                    GameStatus = "day";
+                    GameTime = 302000;
+                    break;
+                case "day":
+                    d_or_n.setText("処刑");
+                    GameStatus = "execution";
+                    GameTime = 62000;
+                    break;
+                case "execution":
+                    d_or_n.setText("よる");
+                    GameStatus = "night";
+                    GameTime = 182000;
+                    break;
+            }
+            CountDown countDown = new CountDown(GameTime, 1000);
+            countDown.start();
+        }
+    }
 }
+
