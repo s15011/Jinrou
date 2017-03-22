efc9cbc858820353084f1bfab151c708b62d9978
nsk0308 <nsk19970308@gmail.com>
2017-03-22 21:06:34 +0900

    修正

--- app/src/main/java/jp/ac/it_college/std/s15011/jinrou/ChatTab.java
+++ app/src/main/java/jp/ac/it_college/std/s15011/jinrou/ChatTab.java
@@ -1,17 +1,18 @@
 package jp.ac.it_college.std.s15011.jinrou;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import static android.R.layout.simple_list_item_1;
 
 /**
  * Created by Nakachi Samuel Kota on 2017/03/20.
@@ -20,17 +21,24 @@ import static android.R.layout.simple_list_item_1;
 public class ChatTab extends Fragment {
 
     ArrayAdapter<String> adapter;
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//    }
-//
+    private InputMethodManager inputMethodManager;
+    private View window;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.chat_tab, container, false);
-////        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "JKG-L_3.ttf");
-////        ((TextView) view.findViewById(R.id.textView)).setTypeface(typeface);
+        final View view = inflater.inflate(R.layout.chat_tab, container, false);
+
+        inputMethodManager = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        window = view.findViewById(R.id.listView);
+        window.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                inputMethodManager.hideSoftInputFromWindow(window.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                window.requestFocus();
+                return false;
+            }
+        });
 
         return view;
     }
@@ -39,14 +47,18 @@ public class ChatTab extends Fragment {
     public void onStart() {
         super.onStart();
 
-        Button button = (Button)getActivity().findViewById(R.id.button2);
+        final Button button = (Button)getActivity().findViewById(R.id.button2);
+        final EditText comment = (EditText)getActivity().findViewById(R.id.editText);
+
         ListView listview = (ListView)getActivity().findViewById(R.id.listView);
-        adapter = new ArrayAdapter<String>(getActivity(), simple_list_item_1);
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.chat_tab_row);
 
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                addStringData();
+                if (!comment.getText().toString().equals("")) {
+                    addStringData();
+                }
             }
         });
 
@@ -55,6 +67,7 @@ public class ChatTab extends Fragment {
 
     private void addStringData(){
         EditText edittext = (EditText)getActivity().findViewById(R.id.editText);
-        adapter.add(edittext.getText().toString());
+        adapter.add("なまえ >> " + edittext.getText().toString());
+        edittext.getEditableText().clear();
     }
 }
